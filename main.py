cfe5f059026e13e623df91a61eb778fc6e330c0e
Gill-Singh-A <amansg22@iitk.ac.in>
2024-10-27 23:12:09 +0530

    Implemented Calling of main Function

--- main.py
+++ main.py
@@ -137,3 +137,11 @@ if __name__ == "__main__":
     arguments.timeout = float(arguments.timeout) if arguments.timeout else None
     if not arguments.write:
         arguments.write = f"{date.today()} {strftime('%H_%M_%S', localtime())}.csv"
+    display('+', f"Total Servers     = {Back.MAGENTA}{len(arguments.server)}{Back.RESET}")
+    display('+', f"Total Credentials = {Back.MAGENTA}{len(arguments.credentials)}{Back.RESET}")
+    t1 = time()
+    successful_logins = main(arguments.server, arguments.credentials, arguments.scheme, arguments.timeout)
+    t2 = time()
+    display(':', f"Successful Logins = {Back.MAGENTA}{len(successful_logins)}{Back.RESET}")
+    display(':', f"Time Taken        = {Back.MAGENTA}{t2-t1:.2f} seconds{Back.RESET}")
+    display(':', f"Rate              = {Back.MAGENTA}{len(arguments.credentials)/(t2-t1):.2f} logins / seconds{Back.RESET}")
